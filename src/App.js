0986bcfdbb4a0a7d8589b7bc8bb77b0786e7cc37
LauraPastor <laurapastorj@gmail.com>
2023-06-21 16:41:35 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button>+ New chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>one</li>
+        </ul>
         <nav>
-          <p>Made by Laura</p>
+          <p>Made by a cyborg called Laura</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>LauraGPT</h1>
+        <ul className="feed">{/* <li></li> */}</ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Free Research Preview. ChatGPT may produce inaccurate information
+            about people, places, or facts. ChatGPT May 24 Version
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
