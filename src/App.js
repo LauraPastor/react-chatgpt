002026379441b2c751db874580d0f164b414e210
LauraPastor <laurapastorj@gmail.com>
2023-09-30 16:16:47 +0200

    guardame

--- src/App.js
+++ src/App.js
@@ -1,22 +1,100 @@
+import { useState, useEffect } from "react";
 const App = () => {
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState([]);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  };
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue("");
+  };
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      // console.log(data);
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message)
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        { title: currentTitle, role: "user", content: value },
+        //this  is the responds from the AI
+        { title: currentTitle, role: message.role, content: message.content },
+      ]);
+  }, [message, currentTitle]);
+
+  console.log(previousChats);
+
+  const currentChat = previousChats.filter(
+    (previousChat) => previousChat.title === currentTitle
+  );
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((previousChats) => previousChats.title))
+  );
+  console.log(uniqueTitles);
+
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
+        <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
-          <li>one</li>
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
+            </li>
+          ))}
         </ul>
         <nav>
           <p>Made by a cyborg called Laura</p>
         </nav>
       </section>
       <section className="main">
-        <h1>LauraGPT</h1>
-        <ul className="feed">{/* <li></li> */}</ul>
+        {!currentTitle && <h1>LauraGPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
+        </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input />
-            <div id="submit">➢</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
           </div>
           <p className="info">
             Free Research Preview. ChatGPT may produce inaccurate information
