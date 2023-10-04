f63452d4c7600d0291ae978f0bdd1f3beb34f9dd
LauraPastor <laurapastorj@gmail.com>
2023-10-04 17:23:54 +0200

    Change submit button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 const App = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(null);
   const [message, setMessage] = useState(null);
   const [previousChats, setPreviousChats] = useState([]);
   const [currentTitle, setCurrentTitle] = useState([]);
